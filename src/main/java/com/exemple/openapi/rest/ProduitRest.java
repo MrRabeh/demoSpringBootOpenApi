b49150bee8e70ed238f4108e20ac7efd3036b575
RABEH TARIK <tarik.rabeh@gmail.com>
2024-01-13 21:03:33 +0100

    API reponse

--- src/main/java/com/exemple/openapi/rest/ProduitRest.java
+++ src/main/java/com/exemple/openapi/rest/ProduitRest.java
@@ -2,7 +2,10 @@ package com.exemple.openapi.rest;
 
 import com.exemple.openapi.metier.model.Produit;
 import com.exemple.openapi.metier.service.ProduitInterface;
-import com.exemple.openapi.metier.service.impl.ProduitImplementation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,11 @@ public class ProduitRest {
     @Autowired
     ProduitInterface produitInterface;
 
+    @Operation(description = "ajout produit")
+    @ApiResponse(responseCode = "200",
+            description = "Ajout Produit OK",
+    content = {@Content(mediaType = "application/json",
+    schema = @Schema(implementation = Produit.class))})
     @PostMapping("/ajout")
     public Produit ajoutProduit(String name,double prix,int quantite){
         return produitInterface.ajout(name,prix,quantite);
